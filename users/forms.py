727d9b2eee66b201f23794a42a797d7580010f90
Rashidalabri0803 <rashid92759346@gmail.com>
2024-11-19 18:30:22 +0000

    1

--- users/forms.py
+++ users/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django import forms
 
 class CustomUserCreationForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
+    class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2', 'user_type', 'profile_picture', 'phone_number', 'address')
         widgets = {
